6816b805fc0be902e2cd68189452476f488d47af
JP Angelle <jpangelle15@gmail.com>
2019-09-25 20:37:46 -0500

    add input support for POST example

--- example/src/useLazyAxios/PostData.tsx
+++ example/src/useLazyAxios/PostData.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLazyAxios } from '../../../src';
 
 export default () => {
+  const [name, setName] = useState('');
+
   const [getData, { data, error, loading }] = useLazyAxios({
     url: 'https://reqres.in/api/users',
     method: 'POST',
   });
-
-  const lazyData = {
-    name: 'morpheus',
-    job: 'leader',
-  };
 
   return (
     <>
       {loading && 'Loading...'}
       {error && error.message}
       {data && !loading && <div>POST Successful</div>}
-      <button type="button" disabled={loading} onClick={() => getData(lazyData)}>
+      {!data && !error && !loading && (
+        <label htmlFor="name">
+          Name:
+          <input
+            id="name"
+            value={name}
+            onChange={event => {
+              setName(event.target.value);
+            }}
+          />
+        </label>
+      )}
+      <button type="button" disabled={loading} onClick={() => getData({ name })}>
         get data
       </button>
     </>
