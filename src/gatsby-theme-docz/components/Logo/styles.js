014a6dba22cfffb80a2602fc284e75c26b71384e
Zak Angelle <zaknotzak@gmail.com>
2019-09-28 13:37:53 -0500

    Add basic documentation and styles to docs

---
+++ src/gatsby-theme-docz/components/Logo/styles.js
@@ -0,0 +1,5 @@
+export const logo = {
+  marginTop: '6px',
+  height: 24,
+  color: '#fff',
+};
---
+++ src/gatsby-theme-docz/components/NavLink/styles.js
@@ -0,0 +1,10 @@
+export const link = {
+  my: 2,
+  display: 'block',
+  color: '#012e3f',
+  textDecoration: 'none',
+  fontSize: 2,
+  '&.active': {
+    fontWeight: 600,
+  },
+};
