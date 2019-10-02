053542f90dc62819f2c0658dfbfc5caf0ae57b88
Zak Angelle <zaknotzak@gmail.com>
2019-10-01 22:56:50 -0500

    Rework eslint and prettier config and scripts

--- docs/src/gatsby-theme-docz/wrapper.js
+++ docs/src/gatsby-theme-docz/wrapper.js
@@ -1,14 +1,15 @@
-import * as React from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet';
 import favicon from './assets/favicon.png';
 
 export default ({ children }) => (
-  <React.Fragment>
+  <>
     <Helmet>
       <meta charSet="utf-8" />
+
       <link rel="icon" type="image/x-icon" href={favicon} />
     </Helmet>
 
     {children}
-  </React.Fragment>
+  </>
 );
