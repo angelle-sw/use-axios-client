9fa4105bc170930abebd1de6662138b80dcbd73e
Zak Angelle <zaknotzak@gmail.com>
2019-10-02 19:06:51 -0500

    Clean up docs and configs

--- docs/src/gatsby-theme-docz/wrapper.js
+++ docs/src/gatsby-theme-docz/wrapper.js
@@ -6,7 +6,7 @@ export default ({ children }) => (
   <>
     <Helmet>
       <meta charSet="utf-8" />
-
+      <meta name="theme-color" content="#012e3f" />
       <link rel="icon" type="image/x-icon" href={favicon} />
     </Helmet>
 
